ca917ef2198dca304cfeac34a11454f9850eb530
Lem <lemazizli7@gmail.com>
2026-02-10 02:40:36 +0400

    Update 3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
     if chr(i) == "q" or chr(i) == "e":
-        continue
-    else:  
-        print("{}".format(chr(i)), end="")
+        continue 
+    print("{}".format(chr(i)), end="")
