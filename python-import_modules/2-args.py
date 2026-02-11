babff14809304aede7c043b5072bb3733c8fa88b
Lem <lemazizli7@gmail.com>
2026-02-12 00:51:01 +0400

    Create 2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    n = len(sys.argv) - 1
+    if n == 0:
+        print("0 arguments.")
+    elif n == 1:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(n))
+    for i in range(1, n + 1):
+        print("{:d}: {}".format(i, sys.argv[i]))
