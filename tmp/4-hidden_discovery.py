203006c594df1abc137a90d22134f61f4dbf2c86
Lem <lemazizli7@gmail.com>
2026-02-13 03:01:26 +0400

    Delete tmp directory

--- tmp/4-hidden_discovery.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import hidden_4
-if __name__ == "__main__":
-    all_names = dir(hidden_4)
-    all_names.sort()
-    for name in all_names:
-        if not name.startswith("__"):
-            print(name)
