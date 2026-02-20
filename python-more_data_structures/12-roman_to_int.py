a4054e4d12007ce338c7fa32357cd772016087e4
Lem <lemazizli7@gmail.com>
2026-02-21 01:39:15 +0400

    Update 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -7,11 +7,14 @@ def roman_to_int(roman_string):
         'C': 100, 'D': 500, 'M': 1000
     }
     total = 0
-    length = len(roman_string)
-    for i in range(length):
-        current_val = roman_dict.get(roman_string[i], 0)
-        if i + 1 < length and roman_dict.get(roman_string[i + 1], 0) > current_val:
-            total -= current_val
+    ln = len(roman_string)
+    for i in range(ln):
+        curr = roman_dict.get(roman_string[i], 0)
+        nxt = 0
+        if i + 1 < ln:
+            nxt = roman_dict.get(roman_string[i + 1], 0)
+        if nxt > curr:
+            total -= curr
         else:
-            total += current_val
+            total += curr
     return total
