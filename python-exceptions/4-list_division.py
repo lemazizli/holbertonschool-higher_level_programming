ce147e6fbe2d242594c60f1d63f5d1363463d130
Lem <lemazizli7@gmail.com>
2026-02-21 18:41:08 +0400

    Fix error messages and variable initialization

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -4,13 +4,13 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         div_result = 0
         try:
-            result = my_list_1[i] / my_list_2[i]
+            div_result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
         except TypeError:
             print("wrong type")
         except IndexError:
-            print("out if range")
+            print("out of range")
         finally:
             new_list.append(div_result)
     return new_list
