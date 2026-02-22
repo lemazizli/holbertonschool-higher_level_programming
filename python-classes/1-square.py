59d5404847adf82ffc27e637f2453f2848e1d70b
Lem <lemazizli7@gmail.com>
2026-02-22 21:10:47 +0400

    Create 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Define a class Square with a private size attribute."""
+
+
+class Square:
+    """Represents a square with a private size."""
+
+    def __init__(self, size):
+        """Initialize the square with a private size attribute."""
+        self.__size = size
