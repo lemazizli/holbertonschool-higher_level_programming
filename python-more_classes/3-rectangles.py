ec8140e20f8d61c3cc684401725a46ecb6531259
lemitor <lemazizli7@gmail.com>
2026-02-28 22:46:54 +0000

    done

---
+++ python-more_classes/3-rectangles.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Define an empty class Rectangle"""
+
+
+class Rectangle:
+    """a reactangle"""
+
+    def __init__(self, width=0, height=0):
+        """main"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """reading func"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setting func"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """reading dunv"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setting func"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """calc area"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """perimeter func"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        """ ele bele bir sey"""
+        if self.width == 0 or self.height == 0:
+            return ""
+        TheRec = []
+        for j in range (self.height):
+            TheRec.append("#" * self.width)
+        return "/n".join(TheRec)
