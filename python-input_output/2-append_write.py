a97df0e4aec47a1a50df3bb4ddad57557f93be99
lemito <lemazizli7@gmail.com>
2026-03-05 06:54:31 +0000

    done

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#1/usr/bin/python3
+"""
+This module is containing append_write func.
+"""
+
+
+def append_write(filename="" ,text=""):
+    """
+    this is append_write functions.
+
+    Parametrs: filename and text.
+    Returns: number of chars written
+    """
+    with open(filename, "a") as file:
+        num_chars_written = file.write(text)
+        return num_chars_written
