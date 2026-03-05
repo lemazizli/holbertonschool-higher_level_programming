039827ba68d6924938de5d80b41aaefbf73fe59b
lemito <lemazizli7@gmail.com>
2026-03-05 08:51:32 +0000

    done

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+func create an object from ajason file 
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """returns a python object from json file"""
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
