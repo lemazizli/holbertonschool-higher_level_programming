823fc282225c9a4e4284b2379b76975866cc8629
lemito <lemazizli7@gmail.com>
2026-03-14 17:29:34 +0000

    done

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- yene commentden zaddan 
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256)
+    score INT
+);
+-- burdada cedvelin icine birseyler yazram
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
