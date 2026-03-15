e9255b12596f2f1b7b38840c508bcd5af9a3a439
lemito <lemazizli7@gmail.com>
2026-03-15 18:28:37 +0000

    done

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
 -- burdada ozumuz yaradurug
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-IDENTIFIED BY 'user_0d_1_pwd';
--- her seyede icaze verrik niye cunki ureyimiz ele istiyir
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
