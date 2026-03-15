be92d2b545f95766f6aa624a237430b65ad0cdca
lemito <lemazizli7@gmail.com>
2026-03-15 19:18:44 +0000

    done

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""Module for Selecting states"""
+"""Lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
 
-if __name__ == '__main__':
-    from sys import argv
-    import MySQLdb
-
+if __name__ == "__main__":
+    # Verilənlər bazasına qoşulma
     db = MySQLdb.connect(
-        user=argv[1],
-        password=argv[2],
-        database=argv[3]
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
     cursor = db.cursor()
-
-    cursor.execute('SELECT * FROM states')
-
-    for state in cursor.fetchall():
-        print(state)
-
-    if cursor:
-        cursor.close()
-    if db:
-        db.close()
+    # Mütləq ORDER BY id ASC olmalıdır
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
