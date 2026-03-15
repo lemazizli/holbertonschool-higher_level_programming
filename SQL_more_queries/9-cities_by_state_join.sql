40c5f2b677b6aedec0966bd212e491ad4ad547c1
lemito <lemazizli7@gmail.com>
2026-03-15 18:57:06 +0000

    done

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,13 @@
+-- comment yazmagdan bezdim besdi day 
+SELECT
+    cities.id,
+    cities.name,
+    states.name
+FROM
+    cities
+JOIN
+    states
+ON
+    states.id = cities.state_id
+ORDER BY
+    cities.id ASC;
