0f1cf92bebe4ec084e18f8919cde83bcf7274c66
lemito <lemazizli7@gmail.com>
2026-03-15 18:52:29 +0000

    done

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- yene nese eliyiriy yeto
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa
-CREATE TABLE IF NOT EXISTS satates (
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
