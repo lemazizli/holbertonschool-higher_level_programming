bd33d4b062c2d014290c47d0d165aec55a329e7e
lemito <lemazizli7@gmail.com>
2026-03-15 19:32:06 +0000

    done

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
-"""
-Lists all cities from the database hbtn_0e_4_usa.
-"""
+"""Lists all cities from the database hbtn_0e_4_usa"""
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    # JOIN istifadə edərək şəhərləri ştat adları ilə birlikdə gətiririk
-    cur.execute("SELECT cities.id, cities.name, states.name \
-FROM cities JOIN states ON cities.state_id = states.id \
-ORDER BY cities.id ASC")
-    rows = cur.fetchall()
-    for row in rows:
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                ORDER BY cities.id ASC""")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
     cur.close()
     db.close()
