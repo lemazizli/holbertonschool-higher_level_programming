8196a1ddf04c15bbef2b9e7ac23cffa655cdbf9e
lemito <lemazizli7@gmail.com>
2026-03-29 18:46:56 +0000

    Done

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < arr.length; i++) {
-  const element = arr[i];
-  console.log(element);
+for (const language of languages) {
+  console.log(language);
 }
