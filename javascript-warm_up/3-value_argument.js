1621f50786c25849ad6b8b585395de545dbb42b6
lemito <lemazizli7@gmail.com>
2026-03-29 18:43:05 +0000

    done

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
+const arg = process.argv[2];
 
-const process = require('process');
-const args = process.argv;
-let count = 0;
-for (count in args) {
-  count++;
-}
-
-if (count >= 3) {
-  console.log(`${args[2]}`);
-} else {
+if (arg === undefined) {
   console.log('No argument');
+} else {
+  console.log(arg);
 }
