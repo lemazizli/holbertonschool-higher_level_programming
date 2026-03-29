ed78425774f3a1562cb0f79e6d1e900c0e38902d
lemito <lemazizli7@gmail.com>
2026-03-29 18:52:45 +0000

    Doje

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
-
-const process = require('process');
-const args = process.argv;
-const n = parseInt(args[2]);
 function factorial (n) {
-  if (isNaN(n) || n < 0) {
+  if (isNaN(n) || n === 0 || n === 1) {
     return 1;
-  } else if (n === 0) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
   }
+  return n * factorial(n - 1);
 }
 
-console.log(factorial(n));
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
