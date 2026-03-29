7ac375a3c9525c757dbe76d0dd1f1d7f35bc078e
lemito <lemazizli7@gmail.com>
2026-03-29 18:51:16 +0000

    Done

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,17 +1,9 @@
 #!/usr/bin/node
-
 function add (a, b) {
-  const process = require('process');
-  const args = process.argv;
-  a = parseInt(args[2]);
-  b = parseInt(args[3]);
-  if (args.length > 2) {
-    let add = 0;
-    add = a + b;
-    console.log(add);
-  } else {
-    console.log(NaN);
-  }
+  return a + b;
 }
 
-add();
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+console.log(add(a, b));
