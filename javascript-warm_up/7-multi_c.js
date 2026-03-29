ba686490fbf135b6eb2178af17af1096931f7ad9
lemito <lemazizli7@gmail.com>
2026-03-29 18:48:08 +0000

    done

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
+const x = parseInt(process.argv[2]);
 
-const process = require('process');
-const args = process.argv;
-const myString = 'C is fun';
-if (args.length >= 2) {
-  for (let i = 0; i < args[2]; i++) {
-    console.log(myString);
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
   }
 }
